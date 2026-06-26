cfcf5b60a9feedc424762ef28de33fb537ee30ec
Jaewon-Hong <hongjaewon0428@gmail.com>
2026-06-26 11:24:18 +0900

    완전탐색, combinations 내장함수

--- Lv_1/소수만들기.py
+++ Lv_1/소수만들기.py
@@ -20,3 +20,20 @@ def solution(nums):
             answer += 1
 
     return answer
+
+
+"""
+
+def solution(nums):
+    from itertools import combinations as cb
+    answer = 0
+    for a in cb(nums, 3):
+        cand = sum(a)
+        for j in range(2, cand):
+            if cand%j==0:
+                break
+        else:
+            answer += 1
+    return answer
+    
+"""
