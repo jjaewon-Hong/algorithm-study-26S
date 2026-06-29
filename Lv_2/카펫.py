8114f0c082a07ef577471f773c2e0d1c606ccb1f
Jaewon-Hong <hongjaewon0428@gmail.com>
2026-06-29 12:51:26 +0900

    완전탐색

---
+++ Lv_2/카펫.py
@@ -0,0 +1,13 @@
+def solution(brown, yellow):
+    total = brown + yellow
+    
+    for h in range(3, total+1) :
+        if total % h == 0 :
+            w = total // h
+            
+            if w>=h :
+                if (w-2) * (h-2) == yellow :
+                    # 테두리 제외 넓이 검증
+                    
+                    return [w,h]
+    
